e96893a0d83a6af6e0a3faac43271bbf93d5f2a0
Uli Kusterer <ulivc@zathras.de>
2016-03-04 10:59:40 +0100

    Initial Commit

---
+++ IFFReader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IFFReader
+//
+//  Created by Uli Kusterer on 04/03/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
